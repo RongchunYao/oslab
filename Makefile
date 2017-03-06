67042641afc96393b079eebd5218e28db3b38585
151110079-Yao Rongchun <15895873878@163.com>
2017-03-06 05:01:22 -0800

    do some io coding

--- Makefile
+++ Makefile
@@ -1,17 +1,12 @@
-mbr: mbr.o
-	ld -m elf_i386 -e start -Ttext=0x7c00 -o mbr mbr.o
-	objcopy --strip-all --only-section=.text --output-target=binary mbr mbr
-	ruby mkmbr.rb mbr
-	qemu-system-i386 mbr
-mbr.o: mbr.S
-	gcc -m32 -c mbr.S
-run:
-	gcc -m32 -c mbr.S
-	ld -m elf_i386 -e start -Ttext=0x7c00 -o mbr mbr.o
-	objcopy --strip-all --only-section=.text --output-target=binary mbr mbr
-	ruby mkmbr.rb mbr
-	qemu-system-i386 mbr
+VPATH = ./boot
 .PHONY: clean
+.PHONY: run 
+
+BOOT= boot
+MAKE= make run
+
+run:
+	cd $(BOOT) && $(MAKE)
 
 clean:
-	rm mbr.o mbr	
+	cd $(BOOT) && make clean
---
+++ boot/main.c
@@ -0,0 +1,50 @@
+typedef struct
+{
+	unsigned int   magic;
+	unsigned char  elf[12];
+	unsigned short type;
+	unsigned short machine;
+	unsigned int   version;
+	unsigned int   entry;
+	unsigned int   phoff;
+	unsigned int   shoff;
+	unsigned int   flags;
+	unsigned short ehsize;
+	unsigned short phentsize;
+	unsigned short phnum;
+	unsigned short shentsize;
+	unsigned short shnum;
+	unsigned short shstrndx;
+} elf_header;
+
+typedef struct
+{
+	unsigned int type;
+	unsigned int off;
+	unsigned int vaddr;
+	unsigned int paddr;
+	unsigned int filesz;
+	unsigned int memsz;
+	unsigned int flags;
+	unsigned int align;
+} p_header;
+
+static __inline uint8_t
+inb(int port)
+{
+	uint8_t data;
+	__asm __volatile("inb %w1,%0" : "=a" (data) : "d" (port));
+	return data;
+}
+
+static __inline void
+outb(int port, uint8_t data)
+{
+	__asm __volatile("outb %0,%w1" : : "a" (data), "d" (port));
+}
+
+
+void main()
+{
+	
+}
